ed17be51ccbc21edf91cd7007574091ecf035042
Piotrek <piotr97kam@gmail.com>
2023-04-15 23:33:15 +0200

    Renamed file

---
+++ InsertionSort.h
@@ -0,0 +1,28 @@
+#ifndef INSERTION_SORT
+#define INSERTION_SORT
+
+#include<stdlib.h>
+
+int* selectionSort(int* table, size_t size)
+{
+    int i=1;
+    int q;   
+    while(i<size)
+    {
+        q = table[i+1];
+        for(int j=i; j>=0; j--)
+        {
+            if(q<table[j])
+            {
+                
+            }
+
+        }
+    }
+}
+ 
+
+
+
+
+#endif
