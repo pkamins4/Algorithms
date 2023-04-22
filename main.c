9a1c377189811267f57f099e31a5691f0acba957
Piotrek <piotr97kam@gmail.com>
2023-04-22 18:20:13 +0200

    edited .gitignore

--- main.c
+++ main.c
@@ -4,16 +4,16 @@
 
 int main()
 {
-    int tab[9] = {1,4,15,16,2,3,7,8,9};
-    for(int i = 0; i<9 ;i++)
+    int tab[11] = {1,6,4,15,4,16,2,3,7,8,44};
+    for(int i = 0; i<11 ;i++)
     {
         printf("%i ", tab[i]);
     }
     printf("\n");
-    mergeSort(tab, 0, 8);
+    mergeSort(tab, 0, 10);
     printf("\n");
 
-    for(int i = 0; i<9 ;i++)
+    for(int i = 0; i<11 ;i++)
     {
         printf("%i ", tab[i]);
     }
